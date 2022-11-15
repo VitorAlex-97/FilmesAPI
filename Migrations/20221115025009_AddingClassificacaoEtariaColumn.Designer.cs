30628ce0bc97a9e06b9db79d27bf6d50b9c830ec
VitorAlex-97 <vitualex97@gmail.com>
2022-11-14 23:53:59 -0300

    feat: Creating migration AddingClassificacaoEtariaColumn

--- Migrations/20221115025009_AddingClassificacaoEtariaColumn.Designer.cs
+++ Migrations/20221115025009_AddingClassificacaoEtariaColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FilmesAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221115025009_AddingClassificacaoEtariaColumn")]
+    partial class AddingClassificacaoEtariaColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -22,6 +22,7 @@ namespace Domain.Models
     public string Genero { get; set; }
     [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
     public int Duracao { get; set; }
+    public int ClassificacaoEtaria { get; set; }
 
     [JsonIgnore]
     public virtual List<Sessao> Sessoes { get; set; }
